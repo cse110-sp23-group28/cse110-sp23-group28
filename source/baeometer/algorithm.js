35f973a329028524f87e93089cbe5d9c9af1dd2a
jenetic <jznnylam@gmail.com>
2023-05-21 00:25:46 -0700

    Add unit tests and modified zodiac function

--- source/baeometer/algorithm.js
+++ source/baeometer/algorithm.js
@@ -16,7 +16,7 @@ function getZodiac(dateString) {
     const days = [21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22];
     const signs = ["Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"];
     let dateParts = dateString.split("-");
-    let month = parseInt(dateParts[1]);
+    let month = parseInt(dateParts[1])-1;
     let day = parseInt(dateParts[2]);
     console.log(month)
 
@@ -65,5 +65,7 @@ function getScore(name1, name2, bday1, bday2) {
 
 // Export for Jest unit testing
 if (typeof module !== 'undefined') {
-    module.exports.getScore = getScore
+    module.exports.sumAscii = sumAscii;
+    module.exports.getScore = getScore;
+    module.exports.getZodiac = getZodiac;
 }
--- source/baeometer/tests/algorithm.test.js
+++ source/baeometer/tests/algorithm.test.js
@@ -1,6 +1,33 @@
+/**
+ * CSE 110 SP23 Team 28, UC Sussy Developers
+ * Date: 05/19/2023
+ * Authors: Jenny Lam
+ * Description: Unit tests for algorithm functions
+ */
+
 const functions = require('../algorithm.js');
 
-test('example input is btwn 0 and 1', () => {
+test('getZodiac outputs correct zodiac 1', () => {
+    const res = functions.getZodiac("2000-1-19");
+    expect(res).toBe("Capricorn");
+});
+
+test('getZodiac outputs correct zodiac 2', () => {
+    const res = functions.getZodiac("2000-11-18");
+    expect(res).toBe("Scorpio");
+});
+
+test('sumAscii outputs correct sum 1', () => {
+    const res = functions.sumAscii("this is a test");
+    expect(res).toBe(1301);
+});
+
+test('sumAscii outputs correct sum 2', () => {
+    const res = functions.sumAscii("");
+    expect(res).toBe(0);
+});
+
+test('getScore outputs score btwn 0 and 1', () => {
     const res = functions.getScore("name1", "name2", "2003-04-11", "2003-09-17");
     expect(res).toBeGreaterThanOrEqual(0);
     expect(res).toBeLessThanOrEqual(1);
