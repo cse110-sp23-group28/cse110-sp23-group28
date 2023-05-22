6d265dba52bbab1815e3044eaf1821add8024638
codyprupp <crupp@ucsd.edu>
2023-05-21 22:31:26 -0700

    got eslint to work I think

---
+++ source/baeometer/jest_example.js
@@ -0,0 +1,41 @@
+/**
+ * CSE 110 SP23 Team 28, UC Sussy Developers
+ * Date: 05/19/2023
+ * Authors: Jenny Lam
+ * Description: Example file to demonstrate Jest.
+ */
+
+/**
+ * Adds a and b
+ * @param {number} a the number a
+ * @param {number} b the number b
+ * @returns {number} sum of a and b
+ */
+function sum(a, b) {
+    return a + b
+}
+
+/**
+ * idk what this does tbh
+ * @param {number} a a
+ * @param {number} b b
+ * @returns {number} nothin
+ */
+function bruh(a, b) {
+    return 'String bruh';
+}
+
+if (typeof module !== 'undefined') {
+    module.exports.sum = sum
+}
+
+
+// # Example Test
+
+// ```
+// const functions = require('../jest_example')
+
+// test('addition works', () => {
+//   expect(functions.sum(1, 2)).toBe(3)
+// })
+// ```
