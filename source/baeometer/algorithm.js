d87704d369c59caec1d06abcd2dca4b354d48a56
codyprupp <crupp@ucsd.edu>
2023-05-22 01:10:45 -0700

    fixed linting

--- source/baeometer/algorithm.js
+++ source/baeometer/algorithm.js
@@ -8,14 +8,15 @@
 /**
  * Returns zodiac sign given date
  * @author Jenny Lam
- * @param {string} date 
+ * @param {string} dateString The string representation of the given date
  * @returns {string} zodiac sign
  */
 function getZodiac(dateString) {
 
     const days = [21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22];
-    const signs = ["Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"];
-    let dateParts = dateString.split("-");
+    const signs = ['Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 
+                   'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
+    let dateParts = dateString.split('-');
     let month = parseInt(dateParts[1])-1;
     let day = parseInt(dateParts[2]);
 
@@ -30,8 +31,8 @@ function getZodiac(dateString) {
 /**
  * Returns sum of ascii values from string
  * @author Jenny Lam
- * @param {*} str 
- * @returns {number}
+ * @param {*} str The string to perform the operation on
+ * @returns {number} The total sum of each character's ascii value
  */
 function sumAscii(str) {
     let sum = 0;
@@ -44,10 +45,10 @@ function sumAscii(str) {
 /**
  * Returns score from 0-1 given names and birthdays
  * @author Jenny Lam
- * @param {string} name1 
- * @param {string} name2 
- * @param {string} bday1 
- * @param {string} bday2 
+ * @param {string} name1 The name of the first person
+ * @param {string} name2 The name of the second person
+ * @param {string} bday1 The birthday of the first person
+ * @param {string} bday2 The birthday of the second person
  * @returns {number} score
  */  
 function getScore(name1, name2, bday1, bday2) {
