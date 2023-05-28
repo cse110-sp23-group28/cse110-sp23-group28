1e3230904d2d205e1329c514c6571603e30b5868
wesleyhuhuhu <31289917+wesleyhuhuhu@users.noreply.github.com>
2023-05-27 18:26:34 -0700

    fixed cardClick.js

--- source/baeometer/cardClick.js
+++ source/baeometer/cardClick.js
@@ -6,16 +6,25 @@ Authors: Wesley Hu and Jackie Piepkorn
 Description: System alert when one of the cards is clicked
 */
 //get card button elements
-var card1 = document.getElementById('card1Button');
-var card2 = document.getElementById('card2Button');
-var card3 = document.getElementById('card3Button');
+// var card1 = document.getElementById('card1Button');
+// var card2 = document.getElementById('card2Button');
+// var card3 = document.getElementById('card3Button');
 
 //event listeners for when each card is clicked
-card1.addEventListener('click', changeBackgroundColor);
-card2.addEventListener('click', changeBackgroundColor);
-card3.addEventListener('click', changeBackgroundColor);
+// card1.addEventListener('click', changeBackgroundColor);
+// card2.addEventListener('click', changeBackgroundColor);
+// card3.addEventListener('click', changeBackgroundColor);
 
 //test function
 function testEvent() {
     window.alert("hi");
 }
+
+window.addEventListener('load', function(){
+    let card1 = document.getElementById('card1Button');
+    let card2 = document.getElementById('card2Button');
+    let card3 = document.getElementById('card3Button');
+    card1.addEventListener('click', testEvent);
+    card2.addEventListener('click', testEvent);
+    card3.addEventListener('click', testEvent);
+})
