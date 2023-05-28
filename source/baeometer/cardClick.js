d487e65e8184869cb64a910cfe110979fd728331
wesleyhuhuhu <31289917+wesleyhuhuhu@users.noreply.github.com>
2023-05-27 17:39:12 -0700

    added some js for cardclick

---
+++ source/baeometer/cardClick.js
@@ -0,0 +1,23 @@
+const card1 = document.getElementById('card1Button');
+const card2 = document.getElementById('card2Button');
+const card3 = document.getElementById('card3Button');
+
+var clicked1 = false;
+var clicked2 = false;
+var clicked3 = false;
+
+card1.addEventListener('click', function() {
+    clicked1 = true;
+});
+
+card1.addEventListener('click', function() {
+    clicked2 = true;
+});
+
+card1.addEventListener('click', function() {
+    clicked3 = true;
+});
+
+function changeBackgroundColor(color) {
+
+}
