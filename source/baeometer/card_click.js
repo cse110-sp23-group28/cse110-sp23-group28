b97ec4230c107a2d0b43dc88b8ec047c223e2687
jackiepiepkorn <54698440+jackiepiepkorn@users.noreply.github.com>
2023-05-29 17:50:53 -0700

    Update card_click.js

--- source/baeometer/card_click.js
+++ source/baeometer/card_click.js
@@ -4,21 +4,11 @@
  * Authors: Wesley Hu and Jackie Piepkorn
  * Description: System alert when one of the cards is clicked
  */
-//get card button elements
-// var card1 = document.getElementById('card1Button');
-// var card2 = document.getElementById('card2Button');
-// var card3 = document.getElementById('card3Button');
-
-//event listeners for when each card is clicked
-// card1.addEventListener('click', changeBackgroundColor);
-// card2.addEventListener('click', changeBackgroundColor);
-// card3.addEventListener('click', changeBackgroundColor);
 
 /**
  * Puts a system alert when a card is clicked
  * @author Jackie Piepkorn, Wesley Hu
  */
-
 function testEvent() {
     window.alert('hi');
 }
