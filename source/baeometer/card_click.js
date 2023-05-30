1cd3bdd8ce34b3721774a4f4ddf5c3fa7b34e8b6
jackiepiepkorn <54698440+jackiepiepkorn@users.noreply.github.com>
2023-05-29 17:31:11 -0700

    Update card_click.js

--- source/baeometer/card_click.js
+++ source/baeometer/card_click.js
@@ -1,10 +1,9 @@
 /**
- 
-CSE 110 SP23 Team 28, UC Sussy Developers
-Date: 05/27/2023
-Authors: Wesley Hu and Jackie Piepkorn
-Description: System alert when one of the cards is clicked
-*/
+ * CSE 110 SP23 Team 28, UC Sussy Developers
+ * Date: 05/27/2023
+ * Authors: Wesley Hu and Jackie Piepkorn
+ * Description: System alert when one of the cards is clicked
+ */
 //get card button elements
 // var card1 = document.getElementById('card1Button');
 // var card2 = document.getElementById('card2Button');
