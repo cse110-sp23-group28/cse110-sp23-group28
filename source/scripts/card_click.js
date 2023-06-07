08dcd0513b593f93d577c6f54261e4e17f851507
wesleyhuhuhu <31289917+wesleyhuhuhu@users.noreply.github.com>
2023-06-06 20:11:56 -0700

    added orange boxshadow to selected card

--- source/scripts/card_click.js
+++ source/scripts/card_click.js
@@ -71,7 +71,7 @@ function changeStyle() {
     var card3 = document.getElementById('card3Button');
     if (cardClicked1) {
         card1.style.transform = 'translateY(-8px)';
-        card1.style.boxShadow = '0px 10px 20px 2px rgba(0, 0, 0, 0.25)';
+        card1.style.boxShadow = '0px 10px 20px 2px rgba(255, 200, 150, 0.6)';
         card2.style.transform = '';
         card2.style.boxShadow = '';
         card3.style.transform = '';
@@ -79,7 +79,7 @@ function changeStyle() {
     }
     if (cardClicked2) {
         card2.style.transform = 'translateY(-8px)';
-        card2.style.boxShadow = '0px 10px 20px 2px rgba(0, 0, 0, 0.25)';
+        card2.style.boxShadow = '0px 10px 20px 2px rgba(255, 200, 150, 0.6)';
         card1.style.transform = '';
         card1.style.boxShadow = '';
         card3.style.transform = '';
@@ -87,7 +87,7 @@ function changeStyle() {
     }
     if (cardClicked3) {
         card3.style.transform = 'translateY(-8px)';
-        card3.style.boxShadow = '0px 10px 20px 2px rgba(0, 0, 0, 0.25)';
+        card3.style.boxShadow = '0px 10px 20px 2px rgba(255, 200, 150, 0.6)';
         card1.style.transform = '';
         card1.style.boxShadow = '';
         card2.style.transform = '';
