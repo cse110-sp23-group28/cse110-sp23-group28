c9d6406c26796352aaaaaeae1d6bf97e8a0785f1
Brandon Reponte <bkreponte@ucsd.edu>
2023-06-11 22:09:01 -0700

    Fixed some dead code and progressed to canvas testing

--- source/tests/e2e.test.js
+++ source/tests/e2e.test.js
@@ -5,6 +5,8 @@
  * Description: End to end testing for experience
  */
 
+const functions = require('../scripts/algorithm.js');
+
 describe('Basic user flow for Website', function () {
     // First, visit baeometer through local host or hosted page
     beforeAll(async function () {
@@ -17,13 +19,120 @@ describe('Basic user flow for Website', function () {
   
     // Next, start baeometer by clicking on start button
     it('Initial Home Page - Click to progress to next page', async function () {
-        await page.$eval('button', function (buttonElement) {
-            const startButton = buttonElement.querySelector('nav')
-                .querySelector('a').querySelector('p');
+        // get reference to button and click it
+        await page.$eval('button > nav > a > p', function (startButton) {
             startButton.click();
         });
+        // match to either local host or hosted page
         await expect(page.url()).toMatch
             (/^((http:\/\/127\.0\.0\.1:5500\/source.*)|baeometer\.com)\/baeometer\.html$/);
+    });
+
+    // User fills out their name and birthday
+    it('Baeometer Name and Birthday - YOU', async function () {
+        // // get reference to name input field and type in it
+        // const nameField = await page.$(
+        //     '#yourNameBirthday #yourNameBirthdayContainer #yourNameBirthdayForm #yourNameForm'
+        // );
+        // await nameField.type('Sussy Gus');
+
+        // // get reference to date input field and input a date
+        // const dateField = await page.$(
+        //     '#yourNameBirthday #yourNameBirthdayContainer #yourNameBirthdayForm #yourBirthdayForm'
+        // );
+        // await dateField.click();
+        // await page.keyboard.type('12-31-2022');
+        // await page.keyboard.press('Enter');
+
+        // // get updated name input
+        // const nameValue = await page.$eval(
+        //     '#yourNameBirthday #yourNameBirthdayContainer #yourNameBirthdayForm #yourNameForm',
+        //     function (nameField) {
+        //         return nameField.value;
+        //     }
+        // );
+
+        // // get updated birthday input
+        // const dateValue = await page.$eval(
+        //     '#yourNameBirthday #yourNameBirthdayContainer #yourNameBirthdayForm #yourBirthdayForm',
+        //     function (dateField) {
+        //         return dateField.value;
+        //     }
+        // );
+
+        // // verify that the information is stored
+        // expect(nameValue).toBe('Sussy Gus');
+        // expect(dateValue).toBe('2022-12-31');
+    });
+
+    // User fills out their bae's name and birthday
+    it('Baeometer Name and Birthday - BAE', async function () {
+        // // get reference to name input field and type in it
+        // const nameField = await page.$(
+        //     '#baeNameBirthday #baeNameBirthdayContainer #baeNameBirthdayForm #baeNameForm'
+        // );
+        // await nameField.type('Thomas Powell');
+
+        // // get reference to date input field and input a date
+        // const dateField = await page.$(
+        //     '#baeNameBirthday #baeNameBirthdayContainer #baeNameBirthdayForm #baeBirthdayForm'
+        // );
+        // await dateField.click();
+        // await page.keyboard.type('01-01-1930');
+        // await page.keyboard.press('Enter');
+
+        // // get updated name input
+        // const nameValue = await page.$eval(
+        //     '#baeNameBirthday #baeNameBirthdayContainer #baeNameBirthdayForm #baeNameForm',
+        //     function (nameField) {
+        //         return nameField.value;
+        //     }
+        // );
+
+        // // get updated birthday input
+        // const dateValue = await page.$eval(
+        //     '#baeNameBirthday #baeNameBirthdayContainer #baeNameBirthdayForm #baeBirthdayForm',
+        //     function (dateField) {
+        //         return dateField.value;
+        //     }
+        // );
+
+        // // verify that the information is stored
+        // expect(nameValue).toBe('Thomas Powell');
+        // expect(dateValue).toBe('1930-01-01');
+    });
+
+    // User clicks the test button to test the inputted couple
+    it('Baeometer Name and Birthday - Test couple', async function () {
+        // // click the test button
+        // const testButton = await page.$eval('#inputNameBirthday #testBtn', function () {
+        //     testButton.click();
+        // });
+        
+        // // get display of canvas
+        // const canvasVisible = await page.$eval('#drawCanvas', function (canvas) {
+        //     return window.getComputedStyle(canvas).display;
+        // });
+
+        // check if canvas is visible to user (i.e. not display: "none")
+        // this does not work?
+        //expect(canvasVisible).toBe("block");
+    });
+
+    // 
+    it('Baeometer Canvas - Test canvas', async function () {
+        // const canvas = await page.$('#drawCanvas #canvasContainer #canvasContent #canvasArea');
+        // await canvas.hover();
+        // await page.mouse.down();
+        // await page.mouse.move(20, 0);
+        // await page.mouse.up();
+
+        // expect(functions.getCanvasPixels(canvas.getContext('2d').getImageData())).toBe(200);
+    });
+
+    // 
+    it('Baeometer Card - Test couple', async function () {
+        return;
     });
 });
   
